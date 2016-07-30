7af9e0392582ab8a4c495bb100499f923c527e03
rhrrahul@gmail.com <rhrrahul@gmail.com>
2016-07-31 03:47:52 +0800

    Initial Commit

---
+++ nebula_corp 1.0Tests/nebula_corp_1_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  nebula_corp_1_0Tests.swift
+//  nebula_corp 1.0Tests
+//
+//  Created by macbookair on 7/31/16.
+//  Copyright © 2016 cybertrendzinc. All rights reserved.
+//
+
+import XCTest
+@testable import nebula_corp_1_0
+
+class nebula_corp_1_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
